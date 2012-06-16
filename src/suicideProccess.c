fb78dbef58790306d2707b32831bb49c29d3eb85
Jorman <tille100@gmail.com>
2012-06-16 01:04:07 -0500

    fixed indentation

--- src/suicideProccess.c
+++ src/suicideProccess.c
@@ -7,16 +7,16 @@
 
 int main(int argc, char *argv[], char *env[]) {
 
-// the generator produces one initial seed, (the cause for the suicide)
+  // the generator produces one initial seed, (the cause for the suicide)
   srand(time(NULL));
   int res, cause = rand()%6;
 
-	if( !cause ) exit(rand()%10);
-	else if( cause == 1 ) res = res/0;
-	else if( cause == 2 ) kill(getpid(), SIGKILL);
-  	else if( cause == 3 ) alarm(random() % 60);
-  	else if( cause == 4 ) __asm__("sti");
-	else if( cause == 5 ) res = *((int*) NULL);
+  if( !cause ) exit(rand()%10);
+  else if( cause == 1 ) res = res/0;
+  else if( cause == 2 ) kill(getpid(), SIGKILL);
+  else if( cause == 3 ) alarm(random() % 60);
+  else if( cause == 4 ) __asm__("sti");
+  else if( cause == 5 ) res = *((int*) NULL);
 
   return 0;
 }
